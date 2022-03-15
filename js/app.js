ae34fe067659f4c8ec2d1989070c93c7926c10b9
doerofthings <doerofthings@gmail.com>
2022-03-15 16:58:25 -0400

    implemented calculator operator functions

---
+++ js/app.js
@@ -0,0 +1,29 @@
+// Your calculator is going to contain functions for all of the basic math operators you typically find on simple calculators, so start by creating functions for the following items and testing them in your browser’s console.
+// add (Done)
+// subtract(Done)
+// multiply (Done)
+// divide
+
+const add = (a, b) => {
+    // return a + b;
+    console.log(a + b)
+}
+add(40,1)
+
+const subtract = (a, b) =>{
+    // return a - b;
+    console.log(a - b)
+}
+subtract(100, 50)
+
+const multiply = (a, b) => {
+    // return a * b;
+    console.log( a * b)
+}
+multiply(25,2)
+
+const divide = (a, b) => {
+    // return a / b;
+    console.log(a / b)
+}
+divide(1000,2)
